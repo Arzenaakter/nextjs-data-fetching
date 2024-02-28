7424a5062f0574de74dcc49a422508e4381785e3
Arzena Akter <arzenaakter@gmail.com>
2024-02-28 21:15:56 +0600

    use revalidate  for ssg

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const PostPage = async() => {
-    const res = await fetch("http://localhost:5000/posts")
+    const res = await fetch("http://localhost:5000/posts", {
+        next:{revalidate:5}
+    })
     const post = await res.json()
     return (
         <div>
             <h1>total post:{post.length} </h1>
-            <div>
-                {post.map((item)=><div key={item.id} className="card w-96 bg-base-100 shadow-xl">
+            <div className='space-y-6'>
+                {post.map((item)=><div key={item.id} className="card w-[80%] bg-gray-100 shadow-xl mx-auto">
   <div className="card-body">
                         <h2 className="card-title">{ item.title}</h2>
                         <p>{ item.description}</p>
